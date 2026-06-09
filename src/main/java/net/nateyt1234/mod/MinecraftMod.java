780e2a9d5146ba7433535d79108ff11f45293b50
NateyT1234 <941919@pdsb.net>
2026-06-09 19:51:59 -0400

    I added an item to the mod

--- src/main/java/net/nateyt1234/mod/MinecraftMod.java
+++ src/main/java/net/nateyt1234/mod/MinecraftMod.java
@@ -1,5 +1,7 @@
 package net.nateyt1234.mod;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.nateyt1234.mod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -32,6 +34,8 @@ public class MinecraftMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -45,7 +49,9 @@ public class MinecraftMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
