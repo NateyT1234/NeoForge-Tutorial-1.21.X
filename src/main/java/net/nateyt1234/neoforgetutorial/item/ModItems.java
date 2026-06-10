13a07a3ffc2b31a56fbc9dcc2c27d0b5125d13a7
NateyT1234 <941919@pdsb.net>
2026-06-09 20:34:49 -0400

    Stack sizes

--- src/main/java/net/nateyt1234/neoforgetutorial/item/ModItems.java
+++ src/main/java/net/nateyt1234/neoforgetutorial/item/ModItems.java
@@ -10,10 +10,10 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MinecraftMod.MOD_ID);
 
     public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(64)));
 
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(64)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
